0e524210f441b1fd18ec86d8b1d8ab07ab4a316f
Pratik Vyas <m@pd.io>
2013-09-05 23:12:00 +0530

    no need to return anything in collect

--- logply/collect.py
+++ logply/collect.py
@@ -8,17 +8,12 @@ config_module = os.environ.setdefault('LOGPLY_CONFIG_MODULE', 'logply.config')
 config = import_module(config_module)
 
 def collect():
-	ret = []
 	for log_name, log_config in config.logs.iteritems():
 		assert_log_config(log_config)
 
 		for input_data in get_stage(log_config, 'input'):
 			filtered_data = get_stage(log_config, 'filter', obj=input_data)
 			dispatched_data = get_stage(log_config, 'output', obj=filtered_data)
-
-		ret.append(json.dumps({log_name: filtered_data}))
-
-	return ret
 
 if __name__ == "__main__":
 	collect()
