7d4072003d061ef696b5a8bcb664c9e184519bc0
Pratik Vyas <m@pd.io>
2013-09-05 21:21:58 +0530

    move common things to utils

--- logply/collect.py
+++ logply/collect.py
@@ -1,26 +1,11 @@
 import os
 import json
+
 from importlib import import_module
+from .utils import assert_log_config, get_stage
 
-config_module = os.environ.setdefault('LOGPLY_CONFIG_MODULE', 'config')
+config_module = os.environ.setdefault('LOGPLY_CONFIG_MODULE', 'logply.config')
 config = import_module(config_module)
-
-def assert_log_config(log_config):
-	assert 'input' in log_config
-	assert 'filter' in log_config
-	assert 'output' in log_config
-
-def get_stage(log_config, stage, **extra_args):
-	method, args = get_stage_method_and_args(log_config, stage)
-	args.update(extra_args)
-	data  = method.do(args)
-	return data
-
-def get_stage_method_and_args(log_config, stage):
-	config = log_config.get(stage)
-	method = import_module(config.get('method'))
-	args = config.get('args')
-	return (method, args)
 
 def collect():
 	ret = []
