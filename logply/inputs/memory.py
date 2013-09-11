f77389a5f67d8e26c51d3c4cb66d59656147ec80
Pratik Vyas <m@pd.io>
2013-09-11 11:17:34 +0530

    send memory input time in utc + datetime string

--- logply/inputs/memory.py
+++ logply/inputs/memory.py
@@ -1,11 +1,11 @@
 import psutil
-import time
+from datetime import datetime
 
 def do(kwargs):
 	params = ['total', 'used', 'free', 'percent']
 	ret = obj_to_dict(psutil.phymem_usage(), params, prefix='physical_memory_')
 	ret.update(obj_to_dict(psutil.virtmem_usage(), params, prefix='virtual_memory_'))
-	ret.update({'timestamp': int(time.time())})
+	ret.update({'timestamp': unicode(datetime.utcnow())})
 	yield ret
 
 def obj_to_dict(obj, attrs, prefix=''):
